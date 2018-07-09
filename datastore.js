f215d1027406f83bb323bfc21822a2a2ead55e04
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-07-09 11:37:00 +0100

    initial commit - Tottenham quiz app

---
+++ datastore.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+      question: 'In 1963 Tottenham Hotspur became the first British team to win a European trophy. Which one?',
+      answers: [
+        'Champions League',
+        'European Cup Winners Cup',
+        'UEFA Cup',
+        'European Super Cup'
+        ],
+      correctAnswer: 'European Cup Winners Cup',
+      fact: 'Tottenham handily beat Atletico Madrid 5-1 with diminutive Terry Dyson scoring the final goal with a 40-yard rocket',
+    },
+    {
+      question: 'In which year were the club formed?',
+      answers: [
+        '1902',
+        '1870',
+        '1925',
+        '1882'
+        ],
+      correctAnswer: '1882',
+      fact: 'When the club was formed they were named Hotspur FC, but two years later they changed to Tottenham Hotspur and Athletic Club. Tottenham first played their home games on Tottenham Marshes until 1885, then Northumberland Park until 1898 when they moved into White Hart Lane',
+    },
+    {
+      question: 'Name the Spurs player who scored in the World Cup final 1966.',
+      answers: [
+        'Jimmy Greaves',
+        'Alan Gilzean',
+        'Alan Mullery',
+        'Martin Peters'
+        ],
+      correctAnswer: 'Martin Peters',
+      fact: 'The ball deflected high into the air and bounced down into the penalty area where Peters rifled home a half-volley. The Germans equalised in the final seconds, though glory would still come the team\'s way with the 4–2 win in extra time', 
+    },
+    {
+      question: 'Who was the manager for Spurs when they lifted the FA Cup in 1991?',
+      answers: [
+        'Peter Shreeves',
+        'Terry Venebles',
+        'David Pleat',
+        'Osvaldo Ardiles'
+        ],
+      correctAnswer: 'Terry Venebles',
+      fact: 'Terry was sacked in 1992 after a bust up with chairman Alan Sugar', 
+    },
+    {
+      question: 'How many goals did Jimmy Greaves score in the League in 1963 to become top scorer?',
+      answers: [
+        '35',
+        '40',
+        '37',
+        '42'
+        ],
+      correctAnswer: '37',
+      fact: 'In the 1962–63 season Greaves scored hat-tricks in victories over Manchester United, Ipswich Town and Liverpool, as well as four goals in a 9–2 win over Nottingham Forest',  
+    },
+    {
+      question: 'What club did David Ginola play for before joining Spurs in 1997?',
+      answers: [
+        'Newcastle United',
+        'Paris Saint-Germain',
+        'Lyon',
+        'Aston Villa'
+        ],
+      correctAnswer: 'Newcastle United',
+      fact: 'Tottenham Hotspur signed Ginola in July 1997 for £2.5 million, where he was joined by Newcastle teammate Les Ferdinand. In 1999, he was named PFA Players\' Player of the Year and FWA Footballer of the Year, while playing for Tottenham Hotspur', 
+    },
+    {
+      question: 'Which one of these Scottish born Spurs players tragically lost his life while playing golf?',
+      answers: [
+        'Bill Brown',
+        'Dave Mackay',
+        'Alan Hutton',
+        'John White'
+        ],
+      correctAnswer: 'John White',
+      fact: 'Known as the "Ghost" White was a member of the 1961 double winning team. He was only 27 and at the peak of his career, when he was killed by a lightning strike while sheltering under a tree during a thunderstorm',
+    },
+    {
+      question: 'Which of these Tottenham legends played the most League games for the club?',
+      answers: [
+        'Gary Mabbutt',
+        'Pat Jennings',
+        'Paul Gascoigne',
+        'Steve Perryman' 
+        ],
+      correctAnswer: 'Steve Perryman',
+      fact: 'Perryman amassed an incredible 655 League games for Tottenham between 1969-86. Mabbutt played 485 times in the League for the club, with Jennings pulling on the goalkeeper\'s shirt 472 times, and Gascoigne played 92 League games',
+    },
+    {
+      question: 'Which other London club was defeated by Spurs to win their fifth FA cup in 1967?',
+      answers: [
+        'Woolwich Wanderers',
+        'Leyton Orient',
+        'Chelsea',
+        'West Ham United'
+        ],
+      correctAnswer: 'Chelsea',
+      fact: 'This rivalry itself dates back from the 1967 FA Cup final, which was the competition\'s first final to be contested between two teams from London, and is thus often dubbed the Cockney Cup Final',  
+    },
+    {
+      question: 'After quitting Spurs in 1992, Gary Lineker finished his career in which country?',
+      answers: [
+        'Spain',
+        'Japan',
+        'England',
+        'France'
+        ],
+      correctAnswer: 'Japan',
+      fact: 'In November 1991, Lineker accepted an offer of a two-year contract from J1 League club Nagoya Grampus Eight. The transfer fee paid to Tottenham Hotspur was £2 million. He officially joined Nagoya Grampus Eight after playing his final game for Spurs on 2 May 1992',  
+    },
+];
---
+++ index.js
@@ -0,0 +1,176 @@
+'use strict';
+let questionNumber = 0;
+let score = 0;
+
+function generateQuestion() {
+  console.log(`'genQ' ran`);
+  if (questionNumber < STORE.length) {
+    return`
+    <img class="graphic" src="graphics/Penalty-Kick.png" alt="penalty kick">
+      <form>
+        <fieldset name="multiple-choice-answers">
+          <legend><h1>${STORE[questionNumber].question}</h1></legend>
+          <input class="option" id="choice-1" type="radio" name="answer" role="button" value="${STORE[questionNumber].answers[0]}"/>   
+          <label for="choice-1">${STORE[questionNumber].answers[0]}</label>
+          <input class="option" id="choice-2" type="radio" name="answer" role="button" value="${STORE[questionNumber].answers[1]}"/>  
+          <label for="choice-2">${STORE[questionNumber].answers[1]}</label>
+          <input class="option" id="choice-3" type="radio" name="answer" role="button" value="${STORE[questionNumber].answers[2]}"/>
+          <label for="choice-3">${STORE[questionNumber].answers[2]}</label>
+          <input class="option" id="choice-4" type="radio" name="answer" role="button" value="${STORE[questionNumber].answers[3]}" required/>   
+          <label for="choice-4">${STORE[questionNumber].answers[3]}</label>  
+        </fieldset>
+        <button type="submit" class="js-answer-button answer-button" role="button">Submit</button>
+      </form> 
+        <ul>
+          <li>Question: ${questionNumber + 1}/10</li>
+          <li>Current Score: ${score}</li>
+        </ul>`;   
+  } else {
+    generateQuizResultPage();
+  }
+  // this function is responsible for generating the correct
+  // question in the sequence
+  console.log('`generateQuestion` ran');
+}
+
+function displayQuestion() {
+  $('.js-question-page').html(generateQuestion());
+  console.log('`displayQuestion` ran');
+  // this function will display the correct question to the browser
+}
+
+function handleStartClicked() {
+  $('.js-start-page').on('click', '.js-start-button', function(event) {
+    console.log('`handleStartEvent` ran');
+    $('.js-start-page').css('display', 'none');
+    // $('.js-result-page').css('display', 'none');
+    $(displayQuestion);
+    $('.js-question-page').css('display', 'block'); 
+  });
+  // this function is responsible for when the user
+  // is ready to begin the quiz and clicks start
+  console.log('`handleStartClicked` ran');
+}
+
+function handleAnswerSubmit() {  
+  $('.js-question-page').on('submit', 'form', function(event) {
+    event.preventDefault();
+    let selectedRadio = $('input:checked');
+    let answer = selectedRadio.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    console.log('`handleAnswerSubmit` ran');
+    if (answer === correctAnswer) {
+      $('.js-question-page').css('display', 'none'),
+      handleCorrectAnswer();
+      $('.js-correct-answer-page').css('display', 'block');
+      } else {
+         $('.js-question-page').css('display', 'none'),
+         handleIncorrectAnswer();
+         $('.js-incorrect-answer-page').css('display', 'block');
+
+      }
+    return false;
+  }); 
+  // this function will decide if the answer is correct or
+  // not
+
+}
+
+function handleIncorrectAnswer() {
+  $('.js-incorrect-answer-page').html(`<img class="graphic" src="graphics/GoalKeeper.png" alt="goal saved">
+           <h1>Wrong!</h1>
+           <h2>The correct answer is ${STORE[questionNumber].correctAnswer}</h2>
+           <p>${STORE[questionNumber].fact}</p>
+           <button class="next-button" type="button">NEXT</button>
+           <ul>
+             <li>Question: ${questionNumber + 1}/10</li>
+             <li>Current Score: ${score}</li>
+           </ul>`)
+  // this function is responsible for generating the 
+  // incorrect answer screen
+  console.log('`handleIncorrectAnswer` ran');
+}
+
+function handleCorrectAnswer() {
+  score++;
+  $('.js-correct-answer-page').html(`<img class="graphic" src="graphics/Goal.png" alt="goal scored">
+           <h1>Correct!</h1>
+           <h2>${STORE[questionNumber].correctAnswer}</h2>
+           <p>${STORE[questionNumber].fact}</p>
+           <button class="next-button type="button">NEXT</button>
+           <ul>
+             <li>Question: ${questionNumber + 1}/10</li>
+             <li>Current Score: ${score}</li>
+           </ul>`);
+  // this function is responsible for generating the 
+  // correct answer screen
+  console.log('`handleCorrectAnswer` ran'); 
+}
+
+function handleNextQuestionClicked() {
+  $('div').on('click', '.next-button', function(event) {
+    console.log('stuff');
+    $('.js-answer-page').css('display', 'none');
+    questionNumber++;
+    console.log('more stuff');
+    $(displayQuestion);
+    $('.js-question-page').css('display', 'block');
+  });
+  // this function is responsible for handling when the
+  // user clicks 'next' on the correct/incorrect answer 
+  // screen
+  console.log('`handleNextQuestionClicked` ran');
+}
+
+function generateQuizResultPage() {
+  if (score >= 7) {
+    $('.js-question-page').css('display', 'none');
+    $('.js-result-page').html(`<h3>EXCELLENT!</h3>
+      <img class="graphic" src="graphics/Cup.png" alt="trophy held up">
+      <p>Your Final Score Is: ${score} out of 10!</p>
+      <p>Consider yourself an expert in the history of the club!</p>
+      <button class="restart-button js-restart-button">Re-start</button>`);
+    $('.js-result-page').css('display', 'block');    
+    } else if (score < 7 && score >= 5) {
+      $('.js-question-page').css('display', 'none');
+      $('.js-result-page').html(`<h3>Well Done!</h3>
+        <img class="graphic" src="graphics/Yellow-Card.png" alt="yellow card">
+        <p>Your Final Score Is: ${score} out of 10!</p>
+        <p>You know more about the illustrious history of this club than most!</p>
+        <button class="restart-button js-restart-button">Re-start</button>`);
+      $('.js-result-page').css('display', 'block');
+    } else {
+      $('.js-question-page').css('display', 'none');
+      $('.js-result-page').html(`<h3>Better Luck Next Time!</h3>
+        <img class="graphic" src="graphics/Red-Card.png" alt="red card">
+        <p>Your Final Score Is: ${score} out of 10!</p>
+        <p>Your knowledge of Tottenhams history is limited but you can improve 
+        by taking this quiz again!</p>
+        <button class="restart-button js-restart-button">Re-start</button>`);
+      $('.js-result-page').css('display', 'block');
+    }
+  // after the final answer has been submitted, display
+  // the results with appropriate messages
+  console.log('`generateResultPage` ran');
+}
+
+function restartQuiz() {
+  $('div').on('click', '.js-restart-button', function(event) {
+    $('.js-result-page').css('display', 'none');
+    $('.js-start-page').css('display', 'block');
+    questionNumber = questionNumber - 10;
+    score = score - score;
+  });
+  // if the user hits the 'Re-Take the quiz' button 
+  //they are taken back to the start of the quiz
+  console.log('`restartQuiz` ran');
+}
+
+function handleQuiz() {
+  $(handleStartClicked);
+  $(handleNextQuestionClicked);
+  $(handleAnswerSubmit);
+  $(restartQuiz);
+}
+
+$(handleQuiz);
